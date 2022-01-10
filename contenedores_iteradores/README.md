e99574586b62eb1e07a3cef43683ccdfcf7e2189
asantini <augusto.santini@mirgor.com.ar>
2022-01-09 22:22:26 -0300

    Mejor organizacion de los archivos. Primer ejemplo de contenedores e iteradores listo. Build system preparado

---
+++ contenedores_iteradores/README.md
@@ -0,0 +1,21 @@
+# Contenedores e iteradores
+
+Ejemplos sobre los distintos tipos de contenedores que propone la STL (Standard template library) junto con el concepto de iteradores en C++.
+
+## Array
+
+## Vector
+
+## Cola
+
+## Pila
+
+## Cola de dos extremos
+
+## Lista simplemente enlazada
+
+## Lista doblemente enlazada
+
+## Set y multiset (ordenado/desordenado)
+
+## Mapa y multimapa (ordenado/desordenado)
---
+++ contenedores_iteradores/array/main.cpp
@@ -0,0 +1,59 @@
+// Inclusion para el uso de la clase std::array
+#include <array>
+
+// Inclusion para el uso de std::cin y std::cout
+#include <iostream>
+
+// Cantidad de numeros a ingresar por el usuario
+#define   NUMEROS_A_INGRESAR    (5)
+
+/**
+ * @brief Punto de entrada de nuestro programa
+ * 
+ * Este programa basico demuestra la utilizacion de la clase std::array
+ * como contenedor de numeros ingresados por consola por el usuario.
+ * Luego se imprimen dichos numeros uno por uno, iterando a traves del
+ * contenedor.
+ * 
+ * @return int Codigo de finalizacion del programa
+ */
+int main() {
+  // Notese que esta clase necesita dos variables de template:
+  // - Tipo de datos que se van a alojar en el array
+  // - Cantidad de datos maximos que se van a alojar en el array
+  std::array<int, NUMEROS_A_INGRESAR> numeros_ingresados;
+
+  // Una vez creado el array, no se puede cambiar ni su tamaño ni el tipo
+  // de dato que el mismo aloja
+
+  // Ingreso de numeros en el array
+  std::cout << "Ingrese " << NUMEROS_A_INGRESAR << " numeros" << std::endl;
+  for (int i = 0; i < NUMEROS_A_INGRESAR; i++) {
+    std::cin >> numeros_ingresados[i];
+  }
+
+  // Mostramos los numeros ingresados uno por uno
+  std::cout << "Numeros ingresados:" << std::endl;
+  // Notese que podemos utilizar el metodo size() de std::array para obtener su tamaño
+  // si es que usaramos el viejo metodo para iterar como lo solemos hacer en C
+  // for (int i = 0; i < numeros_ingresados.size(); i++) {
+  //   std::cout << numeros_ingresados.at(i) << std::endl;
+  // }
+
+  // Usando una nueva posibilidad (mas comoda) con los iteradores de C++
+  for (int v : numeros_ingresados) {
+    std::cout << v << std::endl;
+  }
+
+  // Diferencia entre at() y operator[].
+  // En caso de que el indice accedido sea invalido:
+  // - at() -> arroja una excepcion de tipo std::out_of_range
+  // - operator[] -> Causa comportamiento indefinido
+  try {
+    std::cout << numeros_ingresados.at(numeros_ingresados.size() + 1) << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << "Se atrapo exepcion con contenido: " << e.what() << std::endl;
+  }
+
+  return 0;
+}
